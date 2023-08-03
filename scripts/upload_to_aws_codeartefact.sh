abff826207df07ee4f1b90415f488e67fa0f978f
matthiasblomme <b.matthias@gmail.com>
2023-08-03 13:45:36 +0200

    upload to code artefact

--- scripts/upload_to_aws_codeartefact.sh
+++ scripts/upload_to_aws_codeartefact.sh
@@ -7,18 +7,21 @@ AWS_CA_DOMAIN_OWNER="281885323515"
 AWS_REGION="eu-west-3"
 
 #configure aws credentials
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID$AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set region $AWS_REGION
 
 #login to aws codeartifact
 aws codeartifact get-authorization-token --domain $AWS_CA_DOMAIN --domain-owner $AWS_CA_DOMAIN_OWNER --query authorizationToken --output text > auth.txt
-aws codeartifact login --tool generic --repository $AWS_CA_REPO --domain $AWS_CA_DOMAIN --domain-owner $AWS_CA_DOMAIN_OWNER --region $AWS_REGION --token file://auth.txt
+AWS_PROFILE=./auth.txt
+aws codeartifact login --tool generic --repository $AWS_CA_REPO --domain $AWS_CA_DOMAIN --domain-owner $AWS_CA_DOMAIN_OWNER --region $AWS_REGION
 
 #determine new bar file version
-aws codeartifact get-package-version-metadata --domain $AWS_CA_DOMAIN --domain-owner $AWS_CA_DOMAIN_OWNER --repository $AWS_CA_REPO --format generic --namespace esb-artifacts --package-name $BAR_FILE --query version --output text > version.txt
+aws codeartifact list-package-versions --domain $AWS_CA_DOMAIN --domain-owner $AWS_CA_DOMAIN_OWNER --repository $AWS_CA_REPO --format generic --namespace esb-artifacts --package-name $(basename($BAR_FILE)) --output text > version.txt
 BAR_FILE_VERSION=$(./increment_version.sh ./version.txt)
 
 #upload bar file
-aws codeartifact upload-package --domain $AWS_CA_DOMAIN --domain-owner $AWS_CA_DOMAIN_OWNER --repository $AWS_CA_REPO --format generic --namespace esb-artifacts --package-version $BAR_FILE_VERSION --file $BAR_FILE
+aws codeartifact publish-package-version --domain $AWS_CA_DOMAIN --domain-owner $AWS_CA_DOMAIN_OWNER --repository $AWS_CA_REPO --format generic --namespace esb-artifacts --package-version 1.0.0 --asset-name LegalEntityRegistry.bar --asset-content /home/aceuser/artefact/LegalEntityRegistry.bar --package LegalEntityRegistry.bar --asset-sha256
+
 #cleanup authorization file
 rm auth.txt version.txt
