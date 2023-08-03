af5688de9400785a33c08eaa39f0d73caf36d5d0
matthiasblomme <b.matthias@gmail.com>
2023-08-03 14:12:19 +0200

    upload to code artefact

--- scripts/upload_to_aws_codeartefact.sh
+++ scripts/upload_to_aws_codeartefact.sh
@@ -12,15 +12,42 @@ aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws configure set region $AWS_REGION
 
 #login to aws codeartifact
-aws codeartifact get-authorization-token --domain $AWS_CA_DOMAIN --domain-owner $AWS_CA_DOMAIN_OWNER --query authorizationToken --output text > auth.txt
+aws codeartifact get-authorization-token \
+    --domain $AWS_CA_DOMAIN \
+    --domain-owner $AWS_CA_DOMAIN_OWNER \
+    --query authorizationToken \
+    --output text > auth.txt
 export AWS_PROFILE=./auth.txt
-aws codeartifact login --tool generic --repository $AWS_CA_REPO --domain $AWS_CA_DOMAIN --domain-owner $AWS_CA_DOMAIN_OWNER --region $AWS_REGION
+aws codeartifact login \
+    --tool twine \
+    --repository $AWS_CA_REPO \
+    --domain $AWS_CA_DOMAIN \
+    --domain-owner $AWS_CA_DOMAIN_OWNER \
+    --region $AWS_REGION
 
 #determine new bar file version
-aws codeartifact list-package-versions --domain $AWS_CA_DOMAIN --domain-owner $AWS_CA_DOMAIN_OWNER --repository $AWS_CA_REPO --format generic --namespace esb-artifacts --package-name '$(basename "$BAR_FILE")' --output text > version.txt
+aws codeartifact list-package-versions \
+    --domain $AWS_CA_DOMAIN \
+    --domain-owner $AWS_CA_DOMAIN_OWNER \
+    --repository $AWS_CA_REPO \
+    --format generic \
+    --namespace esb-artifacts \
+    --package-name '$(basename "$BAR_FILE")' \
+    --output text > version.txt
 BAR_FILE_VERSION=$(./increment_version.sh ./version.txt)
 
 #upload bar file
-aws codeartifact publish-package-version --domain $AWS_CA_DOMAIN --domain-owner $AWS_CA_DOMAIN_OWNER --repository $AWS_CA_REPO --format generic --namespace esb-artifacts --package '$(basename "$BAR_FILE")' --package-version $BAR_FILE_VERSION --asset-name '$(basename "$BAR_FILE")' --asset-content $BAR_FILE --asset-sha256 "$(sha256sum $BAR_FILE | awk '{print $1}')"
+aws codeartifact publish-package-version \
+    --domain $AWS_CA_DOMAIN \
+    --domain-owner $AWS_CA_DOMAIN_OWNER \
+    --repository $AWS_CA_REPO \
+    --format generic \
+    --namespace esb-artifacts \
+    --package '$(basename "$BAR_FILE")' \
+    --package-version $BAR_FILE_VERSION \
+    --asset-name '$(basename "$BAR_FILE")' \
+    --asset-content $BAR_FILE \
+    --asset-sha256 "$(sha256sum $BAR_FILE | awk '{print $1}')"
+
 #cleanup authorization file
 rm auth.txt version.txt
