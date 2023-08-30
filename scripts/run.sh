7aff9756d019e98932c001eb2a3b9a31ceec3646
matthiasblomme <b.matthias@gmail.com>
2023-08-30 14:53:01 +0200

    cleanup

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+. /opt/ibm/ace-12/server/bin/mqsiprofile
+
 workDir=/home/aceuser/ace-server
 echo "Creating integration server BUILD_IS from $workDir"
 ibmint optimize server --work-directory  $workDir
