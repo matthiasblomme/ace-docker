5b8a466a9adf0326053a013cd592eb74a56bb2e0
Matthias Blomme <mgt_cloud_idc_bloma2@luminus.be>
2024-01-22 14:21:48 +0100

    fix package name bug

--- scripts/get_gitlab_package_registry_urls.sh
+++ scripts/get_gitlab_package_registry_urls.sh
@@ -14,17 +14,30 @@ input_file="$1"
 # Read the list of package names from the input file into an array
 mapfile -t package_names < "$input_file"
 
+# Function to get version of a specific package
+get_version_of_package() {
+    local package_name=$1
+    for entry in "${package_versions[@]}"; do
+        if [[ $entry == "$package_name:"* ]]; then
+            echo "${entry#*: }"
+            return
+        fi
+    done
+    echo "Package not found"
+}
+
 # Get all latest package versions
-IFS=$'\n' read -r -d '' -a package_versions < <(./get_gitlab_package_registry_latest_version.sh && printf '\0')
+IFS=$'\n' read -r -d '' -a package_versions < <(/home/aceuser/scripts/get_gitlab_package_registry_latest_version.sh && printf '\0')
 
 # Initialize an empty array to store the URLs
 urls=()
 
 # Loop through each package name and retrieve the latest version from CodeArtifact
 for package in "${package_names[@]}"; do
-  # Get the latest version of the package from CodeArtifact
+  name="${package%.bar}"
+  #echo "Getting latest version of $name"
   latest_version=$(get_version_of_package "$name")
-  download_url=${gitlab_base_url}/${gitlab_project_id}/packages/generic/${package_name}/${latest_version}/${name}.bar
+  download_url=${gitlab_base_url}/${gitlab_project_id}/packages/generic/${name}/${latest_version}/${name}.bar
   # Append the URL to the array
   urls+=("    - '$download_url'")
 done
